a44408c2f22101c0649ca961d424a3a3e5005f8a
Marcel Wilson <trenchrats@gmail.com>
2022-01-11 22:41:22 -0600

    Update and rename initHacking.ns to initHacking.js

--- src/initHacking.js
+++ src/initHacking.js
@@ -1,27 +1,19 @@
 // This file will download all the needed files to perform the runHacking routine.
 // These will only go through the actions of weaken/grow/hacking servers.
-const baseUrl = 'https://raw.githubusercontent.com/bandophahita/bitburner/master/src/'
+import { baseUrl, } from 'common.js'
 const filesToDownload = [
-    'common.ns',
-    'mainHack.ns',
-    'spider.ns',
-    'grow.ns',
-    'hack.ns',
-    'weaken.ns',
-    'playerServers.ns',
-    'killAll.ns',
-    'runHacking.ns',
-    'find.ns',
+    'common.js',
+    'mainHack.js',
+    'spider.js',
+    'grow.js',
+    'hack.js',
+    'weaken.js',
+    'playerServers.js',
+    'killAll.js',
+    'runHacking.js',
+    'find.js',
 ]
 const valuesToRemove = ['BB_SERVER_MAP']
-
-function localeHHMMSS(ms = 0) {
-    if (!ms) {
-        ms = new Date().getTime()
-    }
-
-    return new Date(ms).toLocaleTimeString()
-}
 
 /** @param {NS} ns */
 async function areYouSure(ns) {
@@ -44,14 +36,14 @@ async function areYouSure(ns) {
 
 /** @param {NS} ns */
 export async function main(ns) {
-    ns.tprint(`[${localeHHMMSS()}] Starting initHacking.ns`)
+    ns.tprint(`Starting initHacking.js`)
 
     let hostname = ns.getHostname()
 
     if (hostname !== 'home') {
         throw new Exception('Run the script from home')
     }
-    await areYouSure();
+    await areYouSure(ns);
 
     for (let i = 0; i < filesToDownload.length; i++) {
         const filename = filesToDownload[i]
@@ -59,12 +51,12 @@ export async function main(ns) {
         await ns.scriptKill(filename, 'home')
         await ns.rm(filename)
         await ns.sleep(200)
-        ns.tprint(`[${localeHHMMSS()}] Trying to download ${path}`)
+        ns.tprint(`Trying to download ${path}`)
         await ns.wget(path + '?ts=' + new Date().getTime(), filename)
     }
 
     valuesToRemove.map((value) => localStorage.removeItem(value))
 
-    ns.tprint(`[${localeHHMMSS()}] Spawning killAll.ns`)
-    ns.spawn('killAll.ns', 1, 'runHacking.ns')
+    ns.tprint(`Spawning killAll.js`)
+    ns.spawn('killAll.js', 1, 'runHacking.js')
 }
